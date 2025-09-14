892a0edd34390f1bd17255c7297f82ab32e0a6a8
vivupadi <vivupadi@gmail.com>
2025-09-14 23:35:49 +0200

    Rag based

--- Chapter_1(Develop generative AI app in Azure).py
+++ Chapter_1(Develop generative AI app in Azure).py
@@ -13,6 +13,8 @@
 
 #Create a chat client
 
+#AIProjectClient : Its the important component
+
 from azure.identity import DefaultAzureCredential
 from azure.ai.projects import AIProjectClient
 from openai import AzureOpenAI
@@ -43,3 +45,59 @@ try:
 
 except Exception as ex:
     print(ex)
+
+###########
+'''Get started with prompt flow to develop language model apps in the Azure AI Foundry'''
+###########
+
+
+###########
+''' Develop a RAG-based solution with your own data using Azure AI Foundry'''
+###########
+
+
+from openai import AzureOpenAI
+
+# Get an Azure OpenAI chat client
+chat_client = AzureOpenAI(
+    api_version = "2024-12-01-preview",
+    azure_endpoint = open_ai_endpoint,
+    api_key = open_ai_key
+)
+
+# Initialize prompt with system message
+prompt = [
+    {"role": "system", "content": "You are a helpful AI assistant."}
+]
+
+# Add a user input message to the prompt
+input_text = input("Enter a question: ")
+prompt.append({"role": "user", "content": input_text})
+
+# Additional parameters to apply RAG pattern using the AI Search index
+rag_params = {
+    "data_sources": [
+        {
+            "type": "azure_search",
+            "parameters": {
+                "endpoint": search_url,
+                "index_name": "index_name",
+                "authentication": {
+                    "type": "api_key",
+                    "key": search_key,
+                }
+            }
+        }
+    ],
+}
+
+# Submit the prompt with the index information
+response = chat_client.chat.completions.create(
+    model="<model_deployment_name>",
+    messages=prompt,
+    extra_body=rag_params
+)
+
+# Print the contextualized response
+completion = response.choices[0].message.content
+print(completion)
