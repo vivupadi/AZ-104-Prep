a7bf9272be5b6d4fcba97cdefc34ea9d03f20dd8
vivupadi <vivupadi@gmail.com>
2025-09-28 23:51:25 +0200

    Object detection

--- Chapter_4(Develop computer vision solutions in Azure).py
+++ Chapter_4(Develop computer vision solutions in Azure).py
@@ -168,6 +168,42 @@ for prediction in results.predictions:
 
 ###########
 '''Detect objects in images'''
+
+"""
+Needs 2 Custome vision resources:
+1. Azure AI Custome Vision training
+2. Azure AI Custom Vision prediction
+
+Can be used:
+azure-cognitiveservices-vision-customvision
+
+
+
+from msrest.authentication import ApiKeyCredentials
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+
+
+ # Authenticate a client for the prediction API
+credentials = ApiKeyCredentials(in_headers={"Prediction-key": "<YOUR_PREDICTION_RESOURCE_KEY>"})
+prediction_client = CustomVisionPredictionClient(endpoint="<YOUR_PREDICTION_RESOURCE_ENDPOINT>",
+                                                 credentials=credentials)
+
+# Get classification predictions for an image
+image_data = open("<PATH_TO_IMAGE_FILE>", "rb").read()
+results = prediction_client.detect_image("<YOUR_PROJECT_ID>",
+                                           "<YOUR_PUBLISHED_MODEL_NAME>",
+                                           image_data)
+
+# Process predictions
+for prediction in results.predictions:
+    if prediction.probability > 0.5:
+        left = prediction.bounding_box.left
+        top = prediction.bounding_box.top 
+        height = prediction.bounding_box.height
+        width =  prediction.bounding_box.width
+        print(f"{prediction.tag_name} ({prediction.probability})")
+        print(f"  Left:{left}, Top:{top}, Height:{height}, Width:{width}")
+"""
 ###########
 
 
